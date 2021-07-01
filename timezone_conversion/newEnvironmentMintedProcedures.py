2a2426206ae0810379c8b8c3414f39419092f16e
Kevin Zehnder <51463990+k-zehnder@users.noreply.github.com>
2021-07-01 13:47:47 -0700

    Delete newEnvironmentMintedProcedures.py

--- timezone_conversion/newEnvironmentMintedProcedures.py
+++
@@ -1,5 +0,0 @@
-# python3 -m venv env (source env/bin/activate) <--vs code does after next step
-# create new .py file
-# run the empty file and choose python config in vs code
-# pip install --upgrade pip
-# pip install --upgrade setuptools
