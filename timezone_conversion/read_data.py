cac750b5ff9d88dbb601f91bffabb61a1eb6460c
Kevin Zehnder <51463990+k-zehnder@users.noreply.github.com>
2021-07-02 11:32:43 -0700

    Update read_data.py

--- timezone_conversion/read_data.py
+++ timezone_conversion/read_data.py
@@ -1,55 +1,43 @@
-from peewee import *
-import datetime
-import pytz
-from pytz import timezone
-import phonenumbers
-from phonenumbers import geocoder
-from faker import Faker
-import pandas as pd
-
-
-from models import db, Patient
+# Remove any existing database file first
+import os
 from utils import *
 
-# initialize db
-db = SqliteDatabase('simple.db')
-db.connect() # important
+# remove db if exits, because will make duplicates if not each time code is run
+if os.path.exists('simple.db'):
+    print("\nDatabase already exists...removing...\n")
+    os.remove('simple.db')
 
-# query all users and print them
-print("\n[INFO] Querying and printing all users...")
-query_all()
+# Create database and tables
+from models import db, Patient
+db.connect()
+db.create_tables([Patient])
 
-query = (Patient
-         .select(Patient.username, Patient.phone, Patient.timezone, Patient.timestamp, Patient.utc_start, Patient.utc_end)
-         .where(
-            (Patient.timezone == "US/Pacific"),
-            #(Patient.timestamp.between(Patient.utc_start, Patient.utc_end))
-            # Patient.available == True
+# fake users
+numbers = ["16692419870", "16617480240", "14436533745"]
+available = [["3 pm", "7 pm"], ["11 am", "3 pm"], ["11 am", "3 pm"]]
+names = [fake.name() for i in range(len(numbers))]
+
+# format availability
+available = availabilityToUTC(available)
+utc_start = convertLocalStartToUtcStart("US/Pacific", available[0][0])
+utc_end = convertLocalStartToUtcStart("US/Pacific", available[0][1])
+
+
+# add to db all rows of users
+rows = zip(names, numbers)
+for row in rows:
+    p = Patient(
+            username=row[0], 
+            phone=row[1], 
+            timezone=convertNumberToTimeZone(row[1]), 
+            utc_start=utc_start,
+            utc_end=utc_end
+            #duration
+            # timestamp (utc) default
             )
-         )
+    p.save() # each row now stored in database
 
-print("\n[INFO] Querying only US/Pacific users...")
-for (i, row) in enumerate(query):
-   print(i, f"name: {row.username} phone: {row.phone} timezone: {row.timezone} timestamp: {row.timestamp}  utc_start: {row.utc_start} utc_end: {row.utc_end}\n")
-
+# close conn
 db.close()
-# functionalize
-"""def findMatch(number, db_object):
-    #query_all()
-    timeZoneFromNumber = convertToTimeZone(number)
-    query = (Patient
-            .select(Patient.username, Patient.phone, Patient.timezone, Patient.availability, Patient.timestamp)
-         .where(
-             #(Patient.timezone == "US/Pacific")
-            (Patient.timezone == timeZoneFromNumber)
-             # (Patient.UTC_Start < Patient.Timestamp < Patient.UTC_End)
-            # Patient.available == True
-         ))
 
-    print(f"\n[INFO] Querying only {timeZoneFromNumber} users...")
-    for (i, row) in enumerate(query):
-        print(i, f"name: {row.username} phone: {row.phone} timezone: {row.timezone} availability: {row.availability} timestamp: {row.timestamp}\n")
-    db.close()
 
-number = ["16617480240"]
-findMatch(number[0], db)"""
