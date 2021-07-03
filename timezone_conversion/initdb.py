f16afce6048f43c4087a0446532a9929ef448bbe
Kevin Zehnder <51463990+k-zehnder@users.noreply.github.com>
2021-07-03 11:01:27 -0700

    Update initdb.py

--- timezone_conversion/initdb.py
+++ timezone_conversion/initdb.py
@@ -1,4 +1,4 @@
-# Remove any existing database file first
+# imports
 import os
 from utils import *
 
@@ -15,13 +15,13 @@ db.create_tables([Patient])
 # fake users
 numbers = ["16692419870", "16617480240", "14436533745"]
 available = [["3 pm", "7 pm"], ["11 am", "3 pm"], ["11 am", "3 pm"]]
-names = [fake.name() for i in range(len(numbers))]
+names = [fake.name() for i in numbers]
 zones = [convertNumberToTimeZone(i) for i in numbers]
 
-# extract digits from each element of list of lists 
+# extract digits from each element of list of lists in availabile variable 
 fmtAvailable = extractAvailabilityFromList(available)
 
-# use formatted availability list from above
+# use fmtAvailable list of lists from above to get availability times converted to utc as list of lists
 times = formattedAvailabilityListToUTC(zones, fmtAvailable)
 
 # add to db all rows of users
@@ -33,10 +33,6 @@ for idx, row in enumerate(rows):
             timezone=convertNumberToTimeZone(row[1]), 
             utc_start=row[2][0],
             utc_end=row[2][1],
-            #implicit fields
-                # timestamp (utc) default
-            #fields to add
-                #duration
             )
     p.save() # each row now stored in database
 
