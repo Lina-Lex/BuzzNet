21f3c07077bbd387880ae8e4cd3dd423e96a0951
Kevin Zehnder <51463990+k-zehnder@users.noreply.github.com>
2021-07-05 09:55:44 -0700

    Add files via upload

---
+++ graphql_version0/schema.py
@@ -0,0 +1,27 @@
+# flask_sqlalchemy/schema.py
+import graphene
+from graphene import relay
+from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
+from models import db_session, Department as DepartmentModel, Employee as EmployeeModel
+
+
+class Department(SQLAlchemyObjectType):
+    class Meta:
+        model = DepartmentModel
+        interfaces = (relay.Node, )
+
+
+class Employee(SQLAlchemyObjectType):
+    class Meta:
+        model = EmployeeModel
+        interfaces = (relay.Node, )
+
+
+class Query(graphene.ObjectType):
+    node = relay.Node.Field()
+    # Allows sorting over multiple columns, by default over the primary key
+    all_employees = SQLAlchemyConnectionField(Employee.connection)
+    # Disable sorting over this field
+    all_departments = SQLAlchemyConnectionField(Department.connection, sort=None)
+
+schema = graphene.Schema(query=Query)
