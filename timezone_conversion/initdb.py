96d3b852362a4d8481d505cc61356f437061ee0b
Kevin Zehnder <51463990+k-zehnder@users.noreply.github.com>
2021-07-06 17:47:46 -0700

    Update initdb.py

--- timezone_conversion/initdb.py
+++ timezone_conversion/initdb.py
@@ -43,21 +43,3 @@ for d in users:
 
     # close conn
 db.close()
-
-# query all users and print them
-print("\n[INFO] Querying and printing all users Pacific Time...")
-query_all()
-
-query = (Patient
-         .select(Patient.username, Patient.phone, Patient.utc_start, Patient.utc_end)
-         .where(
-            (Patient.timezone == "US/Pacific"),
-            Patient.utc_start > datetime.datetime.utcnow()
-            )
-         )
-
-print("\n[INFO] Querying only US/Pacific users...")
-for (i, row) in enumerate(query):
-   print(i, f"name: {row.username} phone: {row.phone} utc_start: {row.utc_start} utc_end: {row.utc_end}\n")
-
-db.close()
