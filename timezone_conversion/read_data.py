d26051f05cfc92f9dc82203e614dc9b637c4494a
Kevin Zehnder <51463990+k-zehnder@users.noreply.github.com>
2021-07-06 17:51:55 -0700

    Update read_data.py

--- timezone_conversion/read_data.py
+++ timezone_conversion/read_data.py
@@ -2,20 +2,23 @@ from peewee import *
 from models import db, Patient
 from utils import *
 
+# initialize db
+db = SqliteDatabase('respondNoTwilio.db')
+db.connect() # important
+
 # query all users and print them
-print("\n[INFO] Querying and printing all users Pacific Time...")
+print("\n[INFO] Querying and printing all users...")
 query_all()
 
 query = (Patient
-         .select(Patient.username, Patient.phone, Patient.utc_start, Patient.utc_end)
+         .select(Patient.username, Patient.phone, Patient.timezone, Patient.utc_start, Patient.utc_end)
          .where(
             (Patient.timezone == "US/Pacific"),
-            Patient.utc_start > datetime.datetime.utcnow()
             )
          )
 
 print("\n[INFO] Querying only US/Pacific users...")
 for (i, row) in enumerate(query):
-   print(i, f"name: {row.username} phone: {row.phone} utc_start: {row.utc_start} utc_end: {row.utc_end}\n")
+   print(i, f"name: {row.username} phone: {row.phone} timezone: {row.timezone} utc_start: {row.utc_start} utc_end: {row.utc_end}\n")
 
 db.close()
