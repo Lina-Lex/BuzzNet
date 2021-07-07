c2e729e4551b52680b0a73b09484d1776ab1391d
Kevin Zehnder <51463990+k-zehnder@users.noreply.github.com>
2021-07-06 19:37:29 -0700

    Update initdb.py

--- timezone_conversion/initdb.py
+++ timezone_conversion/initdb.py
@@ -14,14 +14,19 @@ db.connect()
 db.create_tables([Patient, Person]) # Person for testing purposes
 
 # # Add sample data
-# datetime.datetime(year, month, day, hour, minute)
+# datetime(year, month, day, hour, minute)
 utc_start = datetime.datetime(2021, 7, 6, 6, 0, 0)
 utc_end = datetime.datetime(2021, 7, 6, 6, 0, 0)
 
 users = [
-    {"username": "Alice", "phone": "16692419870", "utc_start" : utc_start, "utc_end" : utc_end},
-    {"username": "Elizabeth", "phone": "16617480240", "utc_start" : utc_start, "utc_end" : utc_end},
-    {"username": "Tim", "phone": "14436533745",  "utc_start" : utc_start, "utc_end" : utc_end},
+    {"username": "Alice", "phone": "16692419870",
+    "utc_start" : utc_start, "utc_end" : utc_end},
+
+    {"username": "Elizabeth", "phone": "16617480240", 
+    "utc_start" : utc_start, "utc_end" : utc_end},
+
+    {"username": "Tim", "phone": "14436533745",  
+    "utc_start" : utc_start, "utc_end" : utc_end},
 ]
 
 for d in users:
@@ -35,5 +40,5 @@ for d in users:
             )
     p.save() # each row now stored in database
 
-    # close conn
+# close conn
 db.close()
