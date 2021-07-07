5111c75d88831798b4f5eb883374b9669e5e40c4
Kevin Zehnder <51463990+k-zehnder@users.noreply.github.com>
2021-07-06 17:45:35 -0700

    Update read_data.py

--- timezone_conversion/read_data.py
+++ timezone_conversion/read_data.py
@@ -2,23 +2,18 @@ from peewee import *
 from models import db, Patient
 from utils import *
 
-# initialize db
-db = SqliteDatabase('simple.db')
-db.connect() # important
-
 # query all users and print them
 print("\n[INFO] Querying and printing all users...")
 query_all()
 
 query = (Patient
-         .select(Patient.username, Patient.phone, Patient.timezone, Patient.timestamp, Patient.utc_start, Patient.utc_end)
+         .select(Patient.username, Patient.phone, Patient.utc_start, Patient.utc_end)
          .where(
             (Patient.timezone == "US/Pacific"),
+            Patient.utc_start > datetime.datetime.utcnow()
             )
          )
 
 print("\n[INFO] Querying only US/Pacific users...")
 for (i, row) in enumerate(query):
-   print(i, f"name: {row.username} phone: {row.phone} timezone: {row.timezone} timestamp: {row.timestamp}  utc_start: {row.utc_start} utc_end: {row.utc_end}\n")
-
-db.close()
+   print(i, f"name: {row.username} phone: {row.phone} utc_start: {row.utc_start} utc_end: {row.utc_end}\n")
