2ac33cb5c8f46718f90a6d5be46b5f013ff5b055
Kevin Zehnder <51463990+k-zehnder@users.noreply.github.com>
2021-07-06 20:41:45 -0700

    Update read_data.py

--- timezone_conversion/read_data.py
+++ timezone_conversion/read_data.py
@@ -10,15 +10,17 @@ db.connect() # important
 print("\n[INFO] Querying and printing all users...")
 query_all()
 
+
+#now = datetime.datetime.utcnow()
 query = (Patient
-         .select(Patient.username, Patient.phone, Patient.timezone, Patient.utc_start, Patient.utc_end)
+         .select(Patient.username, Patient.phone, Patient.timezone, Patient.timestamp, Patient.utc_start, Patient.utc_end)
          .where(
-            (Patient.timezone == "US/Pacific"),
-            Patient.utc_start > datetime.datetime.utcnow())
-         )
+            Patient.timezone == "US/Pacific", Patient.utc_start > datetime.datetime.utcnow()) 
+            )
 
 print("\n[INFO] Querying only US/Pacific users...")
 for (i, row) in enumerate(query):
-   print(i, f"name: {row.username} phone: {row.phone} timezone: {row.timezone} utc_start: {row.utc_start} utc_end: {row.utc_end}\n")
+   print(i, f"name: {row.username} phone: {row.phone} timezone: {row.timezone} timestamp: {row.timestamp}    utc_start: {row.utc_start} utc_end: {row.utc_end}\n")
 
 db.close()
+
