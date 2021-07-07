e164c3dfc0871405240fc1662300125aee5c4fbf
Kevin Zehnder <51463990+k-zehnder@users.noreply.github.com>
2021-07-07 10:27:12 -0700

    Update app.py

--- graphql_version0/app.py
+++ graphql_version0/app.py
@@ -11,7 +11,7 @@ app.debug = True
 
 # Configs
 # Replace the user, password, hostname and database according to your configuration information
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://someusername:@localhost:5432/book-store-api'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://vincentolagbemide:@localhost:5432/book-store-api'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite3'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True 
@@ -26,8 +26,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, index=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    books = db.relationship('Book', backref='author')
-
+    
     def __init__(self, username, email):
       self.username = username
       self.email = email
@@ -35,23 +34,6 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.id
 
-class Book(db.Model):
-    __tablename__ = 'books'
-
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(256), index=True, nullable=False)
-    description = db.Column(db.Text, nullable=False)
-    year = db.Column(db.Integer, nullable=False)
-    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-
-    def __repr__(self):
-        return '<Book %r>' % self.title % self.description % self.year % self.author_id
-
-# Schema Objects
-class BookObject(SQLAlchemyObjectType):
-    class Meta:
-        model = Book
-        interfaces = (graphene.relay.Node, )
 class UserObject(SQLAlchemyObjectType):
    class Meta:
        model = User
@@ -59,32 +41,9 @@ class UserObject(SQLAlchemyObjectType):
 
 class Query(graphene.ObjectType):
     node = graphene.relay.Node.Field()
-    all_books = SQLAlchemyConnectionField(BookObject)
     all_users = SQLAlchemyConnectionField(UserObject)
 
 schema = graphene.Schema(query=Query)
-
-class AddBook(graphene.Mutation):
-    class Arguments:
-        title = graphene.String(required=True)
-        description = graphene.String(required=True) 
-        year = graphene.Int(required=True) 
-        username = graphene.String(required=True)
-    book = graphene.Field(lambda: BookObject)
-
-    def mutate(self, info, title, description, year, username):
-        user = User.query.filter_by(username=username).first()
-        book = Book(title=title, description=description, year=year)
-        if user is not None:
-            book.author = user
-        db.session.add(book)
-        db.session.commit()
-        return AddBook(book=book)
-
-class Mutation(graphene.ObjectType):
-    add_book = AddBook.Field()
-schema = graphene.Schema(query=Query, mutation=Mutation)
-
 
 # Routes
 app.add_url_rule(
@@ -98,7 +57,7 @@ app.add_url_rule(
 
 @app.route('/')
 def index():
-    return 'Welcome to Book Store Api'
+    return 'Welcome to your Api'
 
 if __name__ == '__main__':
      app.run()
