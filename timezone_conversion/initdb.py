8d772b6d82b2f5a68578f227dc6aa3a581ec10f6
Kevin Zehnder <51463990+k-zehnder@users.noreply.github.com>
2021-07-06 20:41:17 -0700

    Update initdb.py

--- timezone_conversion/initdb.py
+++ timezone_conversion/initdb.py
@@ -1,25 +1,47 @@
-from peewee import *
-from models import db, Patient
+# imports
+import os
 from utils import *
+from timezoneHelperClass import TimeZoneHelper
 
-# initialize db
-db = SqliteDatabase('respondNoTwilio.db')
-db.connect() # important
+# remove db if exits, because will make duplicates if not each time code is run
+if os.path.exists('respondNoTwilio.db'):
+    print("\nDatabase already exists...removing...\n")
+    os.remove('respondNoTwilio.db')
 
-# query all users and print them
-print("\n[INFO] Querying and printing all users...")
-query_all()
+# Create database and tables
+from models import db, Patient, Person
+db.connect()
+db.create_tables([Patient, Person]) # Person for testing purposes
 
+# # Add sample data
+# datetime(year, month, day, hour, minute)
+utc_start = datetime.datetime(2021, 7, 6, 6, 0, 0)
+utc_end = datetime.datetime(2021, 7, 6, 6, 0, 0)
 
-#now = datetime.datetime.utcnow()
-query = (Patient
-         .select(Patient.username, Patient.phone, Patient.timezone, Patient.timestamp, Patient.utc_start, Patient.utc_end)
-         .where(
-            Patient.timezone == "US/Pacific", Patient.utc_start > datetime.datetime.utcnow()) 
+users = [
+    {"username": "Alice", "phone": "16692419870",
+    "utc_start" : utc_start, "utc_end" : utc_end},
+
+    {"username": "Elizabeth", "phone": "16617480240", 
+    "utc_start" : utc_start, "utc_end" : utc_end},
+
+    {"username": "Tim", "phone": "14436533745",  
+    "utc_start" : utc_start, "utc_end" : utc_end},
+]
+
+for d in users:
+    tz = TimeZoneHelper(d["phone"])
+    p = Patient(
+            username=d["username"], 
+            phone=d["phone"],
+            utc_start=d["utc_start"],
+            utc_end=d["utc_end"],
+            timezone=tz.numberToTimeZone()
             )
+    p.save() # each row now stored in database
 
-print("\n[INFO] Querying only US/Pacific users...")
-for (i, row) in enumerate(query):
-   print(i, f"name: {row.username} phone: {row.phone} timezone: {row.timezone} timestamp: {row.timestamp}    utc_start: {row.utc_start} utc_end: {row.utc_end}\n")
-
+# close conn
 db.close()
+
+utc_start = datetime.datetime(2021, 7, 6, 6, 0, 0)
+print(utc_start < datetime.datetime.utcnow())
