08473c70a832d6dbc3db37d7e607814ce70ee059
Kevin Zehnder <51463990+k-zehnder@users.noreply.github.com>
2021-07-08 14:55:22 -0700

    check for find in SMS body

--- timezone_conversion/findMatchSMS.py
+++ timezone_conversion/findMatchSMS.py
@@ -11,7 +11,6 @@ from twilio.rest import Client
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
 
-
 load_dotenv()
 twilio_account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
 twilio_api_key_sid = os.environ.get('TWILIO_API_KEY_SID')
@@ -21,7 +20,7 @@ twilio_client = Client(twilio_api_key_sid, twilio_api_key_secret,
 
 app = Flask(__name__)
 
-# see helper.txt and "webhookSMS.png" for explanation of how to set this up
+# see "/helpers/helper.txt and "helpers/webhookSMS.png" and "helpers/nbrok.png" for explanation of how to set this up
 @app.route("/incoming_sms", methods=['GET', 'POST'])
 def incoming_sms():
     """Send a dynamic reply to an incoming text message"""
@@ -33,12 +32,12 @@ def incoming_sms():
     resp = MessagingResponse()
 
     # Determine the right reply for this message
-    if body == 'hello':
-        resp.message("Hi!")
+    if body == 'find' or 'Find':
+        resp.message("phone")
     elif body == 'bye':
         resp.message("Goodbye")
 
     return str(resp)
-
+ 
 if __name__ == "__main__":
     app.run(debug=True)
