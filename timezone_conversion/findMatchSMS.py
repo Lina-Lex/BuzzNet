dae10fffe14014814e6e1608fce65da38ca30917
Kevin Zehnder <51463990+k-zehnder@users.noreply.github.com>
2021-07-08 16:04:57 -0700

    Update findMatchSMS.py

--- timezone_conversion/findMatchSMS.py
+++ timezone_conversion/findMatchSMS.py
@@ -1,7 +1,7 @@
 ################################
 ######## MUST BE RUN WITH NGROK 
 # >>> ngrok http 5000
-# (925) 291-5450
+# kevins twilio number: (925) 291-5450
 # v0
 ################################
 import os
@@ -21,23 +21,32 @@ twilio_client = Client(twilio_api_key_sid, twilio_api_key_secret,
 app = Flask(__name__)
 
 # see "/helpers/helper.txt and "helpers/webhookSMS.png" and "helpers/nbrok.png" for explanation of how to set this up
-@app.route("/incoming_sms", methods=['GET', 'POST'])
-def incoming_sms():
-    """Send a dynamic reply to an incoming text message"""
-    # Get the message the user sent our Twilio number
-    body = request.values.get('Body', None)
-    #phone = request.values.get('Phone')
+@app.route("/incoming_sms", methods=['POST'])
+def sms_reply():  
+    """Respond to incoming calls with a simple text message."""
+ 
+    # Use this data in your application logic
+    from_number = request.form['From']
+    to_number = request.form['To']
+    body = request.form['Body']
 
     # Start our TwiML response
     resp = MessagingResponse()
 
+    # timezone helper class to get time zone from number
+    tz = TimeZoneHelper(from_number)
+
     # Determine the right reply for this message
     if body == 'find' or 'Find':
-        resp.message("phone")
+        resp.message("Your Phone Number: {} \nYour timezone: {}".format(from_number, tz.numberToTimeZone()))
     elif body == 'bye':
         resp.message("Goodbye")
-
+    else:
+        resp.message("I didn't catch that. Sorry.")
+    #elif "alex" in body:
+        #while True:
+            # resp.message(alex) :)
     return str(resp)
- 
+
 if __name__ == "__main__":
     app.run(debug=True)
