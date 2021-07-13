604457686ccacae8f888ea95773267b1476e751b
Kevin Zehnder <51463990+k-zehnder@users.noreply.github.com>
2021-07-13 07:19:52 -0700

    Update production_app.py

--- timezone_conversion/production_app.py
+++ timezone_conversion/production_app.py
@@ -61,3 +61,35 @@ def incoming_sms():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
+@app.route("/incoming_sms", methods=['POST'])
+def sms_reply():  
+    """Determine Users Phone Number from SMS and use TimeZoneHelper class to determine Time Zone."""    
+    from_number = request.form['From'] 
+    body = request.form['Body']
+    to_number = request.form['To']
+
+    # TODO: difference between request.form['Body'] and request.values.get('Body', None)
+    #body = request.values.get('Body', None)
+
+    print(body)
+    # Start our TwiML response
+    resp = MessagingResponse()
+
+    # timezone helper class to get time zone from number
+    tz_from = TimeZoneHelper(from_number)
+    tz_to = TimeZoneHelper(to_number)
+
+    # Determine the right reply for this message
+    if body == 'hello':
+        resp.message("Hi!")
+    elif body == 'bye':
+        resp.message("Goodbye")
+    # Determine the right reply for this message
+    elif body == 'find':
+        resp.message(f"\nYour Phone Number: {from_number} \nYour time zone: {tz_from.numberToTimeZone()} My Number: {to_number} My time zone: {tz_to.numberToTimeZone()}")
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
