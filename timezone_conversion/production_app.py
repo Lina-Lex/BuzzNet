f86d8dab14c94e1e09bd2cff95d7e55e3ac3942d
Kevin Zehnder <51463990+k-zehnder@users.noreply.github.com>
2021-07-13 07:01:00 -0700

    Update production_app.py

--- timezone_conversion/production_app.py
+++ timezone_conversion/production_app.py
@@ -1,1 +1,40 @@
+import os
+from os import environ
+from dotenv import load_dotenv
+from twilio.rest import Client
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
 
+load_dotenv()
+twilio_account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+twilio_api_key_sid = os.environ.get('TWILIO_API_KEY_SID')
+twilio_api_key_secret = os.environ.get('TWILIO_API_KEY_SECRET')
+twilio_client = Client(twilio_api_key_sid, twilio_api_key_secret,
+                       twilio_account_sid)
+
+app = Flask(__name__)
+
+# timezone helper class to get time zone from number
+from timezoneHelperClass import TimeZoneHelper
+
+app = Flask(__name__)
+
+@app.route("/incoming_sms", methods=['GET', 'POST'])
+def incoming_sms():
+    """Send a dynamic reply to an incoming text message"""
+    # Get the message the user sent our Twilio number
+    body = request.values.get('Body', None)
+
+    # Start our TwiML response
+    resp = MessagingResponse()
+
+    # Determine the right reply for this message
+    if body == 'hello':
+        resp.message("Hi!")
+    elif body == 'bye':
+        resp.message("Goodbye")
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
