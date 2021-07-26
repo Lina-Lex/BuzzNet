84111278e824c6fe19e5ba65e6cba80c51c58d74
Kevin Zehnder <51463990+k-zehnder@users.noreply.github.com>
2021-07-25 19:43:26 -0700

    function to backup/dump postgres db pg_pump

---
+++ googlesheets_postgres/simple_postgres_manager.py
@@ -0,0 +1,41 @@
+from subprocess import PIPE,Popen
+import shlex
+import gzip
+
+import os
+import subprocess
+import datetime
+
+DB_NAME = 'goanddo'  # your db name
+DB_USER = 'postgres' # you db user
+DB_HOST = "localhost"
+DB_PORT = 5432
+DB_PASSWORD = os.environ['postgreSQLpass'] # your db password
+
+def dump_schema(DB_HOST, DB_PORT, DB_USER, DB_NAME, **kwargs):
+    dump_success = 1
+    command = f'pg_dump --host={DB_HOST} ' \
+            f'--port={DB_PORT} ' \
+            f'--username={DB_USER} ' \
+            f'--dbname={DB_NAME} ' \
+            f'--no-owner ' \
+            f'--no-password ' \
+            f'--format=c ' \
+            f'--file=pgbackup`date +%F-%H%M`.dump '
+    try:
+        proc = subprocess.Popen(command, shell=True, env={
+                    'PGPASSWORD': DB_PASSWORD
+                    })
+        proc.wait()
+
+    except Exception as e:
+        dump_success = 0
+        print('Exception happened during dump %s' %(e))
+
+
+    if dump_success:
+        print('db dump successfull')
+        #print('restoring to a new database database')
+
+
+# dump_schema(DB_HOST, DB_PORT, DB_USER, DB_NAME)
