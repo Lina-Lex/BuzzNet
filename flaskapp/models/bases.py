3b99be01ef7a80da0bee59ab026074e9e4026e25
Dmitry E. Kislov <kislov@easydan.com>
2021-10-02 12:59:18 +0300

    typo fixed (postgress -> postgres)

--- flaskapp/models/bases.py
+++ flaskapp/models/bases.py
@@ -3,7 +3,7 @@
 
 import datetime
 from peewee import Proxy, Model, DateTimeField
-from flaskapp.models.storages import postgress_db
+from flaskapp.models.storages import postgres_db
 
 db_proxy = Proxy()
 
@@ -14,17 +14,13 @@ class BaseModel(Model):
         database = db_proxy  # connection with database
 
 
-class DatesMixin(Model):
+class DatesMixin(BaseModel):
     updated   = DateTimeField(null=True)  # noqa: E221
     created   = DateTimeField(            # noqa: E221
         default=datetime.datetime.now,
         null=True
     )
 
-
 # NOTE:  Dynamic db-switching is absolutely unnecessary
 # However, this construction will be useful when testing
-db_proxy.initialize(postgress_db)
-
-
-
+db_proxy.initialize(postgres_db)
