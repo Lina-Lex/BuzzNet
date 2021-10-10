cffb1336ab0115f994a50927a41dd861e5d72dc2
Dmitry E. Kislov <kislov@easydan.com>
2021-10-10 18:56:14 +0300

    separate dialog texts and code

---
+++ flaskapp/dialogs/greetings.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+"""
+This file is a part of heartvoices.org project.
+
+The software embedded in or related to heartvoices.org
+is provided under a some-rights-reserved license. This means
+that Users are granted broad rights, including but not limited
+to the rights to use, execute, copy or distribute the software,
+to the extent determined by such license. The terms of such
+license shall always prevail upon conflicting, divergent or
+inconsistent provisions of these Terms. In particular, heartvoices.org
+and/or the software thereto related are provided under a GNU GPLv3 license,
+allowing Users to access and use the software’s source code.
+Terms and conditions: https://www.goandtodo.org/terms-and-conditions
+
+Created Date: Sunday October 10th 2021
+Author: GO and to DO Inc
+E-mail: heartvoices.org@gmail.com
+-----
+Last Modified:
+Modified By:
+-----
+Copyright (c) 2021
+"""
+
+THANKS_FOR_JOIN = """
+Thanks for joining us.
+We are glad to welcome you to our social network.
+Heart Voices is a unique platform where you can make friends with the same interests.
+With us, you can use Google search through your phone.
+You can take advantage of the unique smart reminder feature.
+Our system will remind you every day of important events for you, it can be a daily medication intake, the need to do something, or a reminder that you really want to learn a new language.
+At your request, we will remind you to measure blood pressure or blood sugar, and we will collect this data for you. You can use them when you visit your doctor if needed.
+We provide social support through friendly calls to friends and our operators.
+After forwarding call you will access to our community.
+"""
+
+
+WELCOME_GREETING = "Welcome to Heart Voices ! We help people change their habits on their way to a healthy life without heart disease. Do you want to join us? Say yes or no."
