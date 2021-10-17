10f2eec051eefb670ea1b86f196a1d168447d7af
Dmitry E. Kislov <kislov@easydan.com>
2021-10-17 20:03:10 +0300

    client fixture defined

--- flaskapp/tests/test_api.py
+++ flaskapp/tests/test_api.py
@@ -18,38 +18,71 @@ Created Date: Saturday October 2nd 2021
 Author: GO and to DO Inc
 E-mail: heartvoices.org@gmail.com
 -----
-Last Modified: Saturday, October 2nd 2021, 2:56:01 pm
+Last Modified: Sunday, October 17th 2021, 2:45:05 pm
 Modified By: GO and to DO Inc
 -----
 Copyright (c) 2021
 """
 
+
+import pytest
 import time
 from flaskapp.views.ivrflow import get_term_cond, get_privacy
-from flaskapp import app
-from flask import Response
+from flaskapp import create_app
+from flask import Response, url_for
 
 
+@pytest.fixture(scope='module')
+def client():
+    flask_app = create_app()
+
+    # Create a test client using the Flask application configured for testing
+    with flask_app.test_client() as testing_client:
+        # Establish an application context
+        with flask_app.app_context():
+            yield testing_client  # this is where the testing happens!
+
+
+@pytest.mark.usefixtures("client")
 def test_get_term_cond():
-    with app.app_context():
-        result = get_term_cond()
+    result = get_term_cond()
     assert isinstance(result, Response) is True
     assert result.status_code == 200
 
 
+@pytest.mark.usefixtures("client")
 def test_get_privacy():
-    with app.app_context():
-        result = get_privacy()
+    result = get_privacy()
     assert isinstance(result, Response) is True
     assert result.status_code == 200
 
 
+@pytest.mark.usefixtures("client")
 def test_get_privacy_caching():
-    with app.app_context():
-        start = time.time()
-        result1 = get_privacy()
-        end = time.time()
-        result2 = get_privacy()
-        final = time.time()
+    start = time.time()
+    result1 = get_privacy()
+    end = time.time()
+    result2 = get_privacy()
+    final = time.time()
     assert final - end >= end - start
     assert result1 == result2
+
+
+def test_save_client_type(monkeypatch):
+    def mocked_google_proxy_obj():
+        ...
+    mocked_google_proxy_obj.append_row_to_sheet = lambda x: None
+    mocked_google_proxy_obj.update_cell = lambda x, y, z, w: None
+
+    monkeypatch.setattr(
+        'flaskapp.core.ivr_core.gs_users_existing',
+        mocked_google_proxy_obj
+    )
+
+    # with app.app_context():
+    #     save_client_url = url_for('save_client_type')
+    #     breakpoint()
+    #     assert False
+
+    #     with app.test_client() as api_client:
+    #         ...
